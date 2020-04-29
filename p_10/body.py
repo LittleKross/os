50229ef5b49b04c9aa23030e41c2f2cb4ac6d9a9
Brandon Young <btyoung2k@gmail.com>
2020-04-29 19:39:02 -0400

    adding beginning of functionality

--- p_10/body.py
+++ p_10/body.py
@@ -1,1 +1,27 @@
-:
+import sys
+import os
+
+def checkArg(inputPath):
+        print("Input path")
+        print(inputPath)
+        print(os.environ.get('PATH'))
+
+        print("Currently Looknig for: " + inputPath[1])
+        searchValue = inputPath[1]
+        PATH = os.environ.get('PATH')
+        dirs = PATH.split(';')
+        for i in range(0,dirs.__len__()-1):
+            testPath = os.path.join(dirs[i],searchValue)
+            print(testPath)
+            if os.path.exists(testPath):
+                print("true")
+            #os.chdir(dirs[i])
+            #children = os.getcwd()
+            #print(children)
+            #for j in range(0,children.__len__()-1):
+            #    print(children[j])
+            #if()
+            #print(dirs[i])
+
+pathSearch = sys.argv
+checkArg(pathSearch)
