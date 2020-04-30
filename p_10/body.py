7d43345fdb7bf37d8e483691bf1adcf53007ebd1
Brandon Young <btyoung2k@gmail.com>
2020-04-29 20:11:21 -0400

    adding walk to body.py

--- p_10/body.py
+++ p_10/body.py
@@ -2,19 +2,24 @@ import sys
 import os
 
 def checkArg(inputPath):
-        print("Input path")
-        print(inputPath)
-        print(os.environ.get('PATH'))
+        #print("Input path")
+        #print(inputPath)
+        #print(os.environ.get('PATH'))
 
         print("Currently Looknig for: " + inputPath[1])
         searchValue = inputPath[1]
         PATH = os.environ.get('PATH')
         dirs = PATH.split(';')
         for i in range(0,dirs.__len__()-1):
-            testPath = os.path.join(dirs[i],searchValue)
-            print(testPath)
-            if os.path.exists(testPath):
-                print("true")
+            testPath = dirs[i]
+            for folderName, subfolders, filenames in os.walk(testPath):
+                #print('The current folder is ' + folderName)
+                for filename in filenames:
+                    #print('FILE INSIDE ' + folderName + ': '+ filename)
+                    if filename == searchValue:
+                        found = os.getcwd()
+                        print(found)
+                    #print('')
             #os.chdir(dirs[i])
             #children = os.getcwd()
             #print(children)
